f19038aa1310bdaf7efc35f56df05e9ce629b1b4
andrew cohen <cohen.andrew.n@gmail.com>
2025-01-22 22:15:59 -0500

    feat: add more stuff

--- uxplore/src/routes/glow-hover-box.tsx
+++ uxplore/src/routes/glow-hover-box.tsx
@@ -1,20 +1,63 @@
 import { createEffect, createSignal } from "solid-js"
 
-export default function Home() {
-    function moveBox(event: MouseEvent) {
-        var d = document.getElementById('box-to-move')
-        console.log(event)
-        if (d) {
-            d.animate({
-                left:`${event.clientX}px`,
-                top: `${event.clientY}px`,
-            }, {duration: 1000, fill: "forwards"})
-        }
+const glowSize = 100
+
+function moveGlow(event: MouseEvent) {
+    var d = document.getElementById('glow')
+
+    const div_x = event.clientX - (glowSize / 2)
+    const div_y = event.clientY - (glowSize / 2)
+
+    if (d) {
+        d.style.display = 'block'
+        d.animate({
+            left:`${div_x}px`,
+            top: `${div_y}px`,
+        }, {duration: 1000, fill: "forwards"})
     }
+}
+
+
+function clearGlow(event: MouseEvent) {
+    var d = document.getElementById('glow')
+
+    if (d) {
+        d.style.display = "none"
+    }
+}
+
+export default function Home() {
 
     return (
-        <div class="h-screen w-screen" onMouseMove={moveBox}>
-            <div class="h-8 w-8 bg-amber-800 absolute" id='box-to-move'></div>
-        </div>
+        <main class="h-screen w-screen p-4 flex justify-center items-center" >
+            <div class="flex flex-wrap gap-2">
+                <Box 
+                title="Repository"
+                description="Self-authenticating storage for public account content."
+                />
+                <Box 
+                title="Repository"
+                description="Self-authenticating storage for public account content."
+                />
+                <Box 
+                title="Repository"
+                description="Self-authenticating storage for public account content."
+                />
+            </div>
+            <div 
+            style={{height: `${glowSize}px`, width: `${glowSize}px`, filter:"blur(25px)"}}
+            class="rounded-full bg-amber-800 absolute" id="glow"></div>
+        </main>
     )
 } 
+
+function Box(props: {title: string, description: string}) {
+    return (
+        <div class={"rounded-lg border border-black p-2 flex flex-col space-y-1"}on:mousemove={moveGlow}>
+            <h1 class={"text-2xl font-bold"}>
+                {props.title}
+            </h1>
+            <p>{props.description}</p>
+        </div>
+    )
+}
